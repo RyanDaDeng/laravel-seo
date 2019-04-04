f361d3ba413ba4be9733aef81a105cc7bd5d6566
rayndeng <ryan@tonercity.com.au>
2019-04-05 10:14:09 +1100

    finished

---
+++ database/migrations/2019_04_05_095547_add_group_by_index_to_tbl_gw_query_details_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGroupByIndexToTblGwQueryDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tbl_gw_query_details', function (Blueprint $table) {
+            //
+            $table->dropIndex('group_by_index_keyword_page_index');
+            $table->dropIndex('tbl_gw_query_details_index');
+            $table->dropIndex('keyword_page_date_index');
+            $table->dropIndex('date_index');
+            $table->dropIndex('page_keyword_index');
+            $table->dropIndex('covering_index');
+
+            $table->index(['index', 'keyword', 'page'], 'group_by_index_keyword_page_index');
+            $table->index(['index', 'page', 'keyword', 'date', 'device'], 'tbl_gw_query_details_index');
+            $table->index(['index', 'page', 'keyword', 'date'], 'keyword_page_date_index');
+            $table->index(['date'], 'date_index');
+            $table->index(['index', 'page', 'keyword'], 'page_keyword_index');
+            $table->index(['index', 'page', 'keyword', 'date', 'clicks', 'impressions', 'position', 'average_weight_ranking'], 'covering_index');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tbl_gw_query_details', function (Blueprint $table) {
+            //
+//            $table->dropIndex('group_by_index_keyword_page_index');
+//            $table->dropIndex('tbl_gw_query_details_index');
+//            $table->dropIndex('keyword_page_date_index');
+//            $table->dropIndex('date_index');
+//            $table->dropIndex('page_keyword_index');
+//            $table->dropIndex('covering_index');
+        });
+    }
+}
